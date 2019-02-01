e3ddeea32a7ce9936ce12b045248b4538ac345f4
Luis Mejia <lcmejia19@gmail.com>
2019-01-31 20:10:25 -0600

    fix: migration bug

--- db/migrate/20190201020829_create_quotations.rb
+++ db/migrate/20190201020829_create_quotations.rb
@@ -6,7 +6,8 @@ class CreateQuotations < ActiveRecord::Migration[5.2]
       t.decimal :ask_value
       t.decimal :max_bid
       t.decimal :min_bid
-      t.decimalvariation_percent_bid :variation_bid
+      t.decimal :variation_bid
+      t.decimal :variation_percent_bid
       t.decimal :open_bid_value
       t.datetime :date
       t.datetime :created_at
---
+++ db/schema.rb
@@ -0,0 +1,42 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2019_02_01_020829) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "currencies", force: :cascade do |t|
+    t.string "name"
+    t.string "code"
+    t.string "flag"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "quotations", force: :cascade do |t|
+    t.bigint "currency_id"
+    t.decimal "bid_value"
+    t.decimal "ask_value"
+    t.decimal "max_bid"
+    t.decimal "min_bid"
+    t.decimal "variation_bid"
+    t.decimal "variation_percent_bid"
+    t.decimal "open_bid_value"
+    t.datetime "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["currency_id"], name: "index_quotations_on_currency_id"
+  end
+
+  add_foreign_key "quotations", "currencies"
+end
