9504aebaea64c5229786874da5379fddf6b175b8
Luis Mejia <lcmejia19@gmail.com>
2019-02-02 10:36:10 -0600

    add protect_from_forgery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :set_global_settings
+
+  def set_global_settings
+    @global_decimal = "%.#{@global_truncate}f"
+  end
 end
