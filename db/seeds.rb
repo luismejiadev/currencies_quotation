d0aa10f02ddced2ab09a51d89af9c93e0b9abec0
Luis Mejia <lcmejia19@gmail.com>
2019-02-02 12:56:08 -0600

    add global seeds to Load Currencies initial records

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Currency.create!([
+   {:name => 'Euro', :code => 'EUR'},
+   {:name => 'US Dollar', :code => 'USD'},
+   {:name => 'Australian dollar', :code => 'AUD'},
+   {:name => 'Peso Argentina', :code => 'ARS'}
