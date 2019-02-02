89d079ca9ff1aa780696083e8be15d12cbafd084
Luis Mejia <lcmejia19@gmail.com>
2019-02-02 12:55:27 -0600

    add intertal to Quotation model

---
+++ db/migrate/20190202183141_add_interval_to_quotations.rb
@@ -0,0 +1,5 @@
+class AddIntervalToQuotations < ActiveRecord::Migration[5.2]
+  def change
+    add_column :quotations, :interval, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_01_020829) do
+ActiveRecord::Schema.define(version: 2019_02_02_183141) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2019_02_01_020829) do
     t.datetime "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "interval"
     t.index ["currency_id"], name: "index_quotations_on_currency_id"
   end
 
