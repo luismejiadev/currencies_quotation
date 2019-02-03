bedfa0da13dcea77f12b6c41716ed8d03f541f1a
Luis Mejia <lcmejia19@gmail.com>
2019-02-02 18:11:51 -0600

    add serializer to format API json values

--- app/models/quotation.rb
+++ app/models/quotation.rb
@@ -1,3 +1,15 @@
 class Quotation < ApplicationRecord
   belongs_to :currency
+  def self.json_serialize(rows)
+    data = []
+    rows.each do |obj|
+      values = {
+        bid_value: obj.bid_value.to_f,
+        ask_value: obj.ask_value.to_f,
+        date: obj.date.strftime("%H")
+      }
+      data.push(values)
+    end
+    return data.to_json
+  end
 end
